fa89d7bd5252f8fb70f6c6e5d6ab5f99065a54dc
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-21 13:25:46 -0500

    Add tilt and rotate methods

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,65 +1,78 @@
-class Game < ActiveRecord::Base
+class Game
+ # < ActiveRecord::Base
 
   def winner?(board_string)
     board = transform_into_2d(board_string)
     winner = nil
 
+    show(board)           { puts "Checking rows..." }
     winner ||= check_rows(board)
-    [board, transform(board, -1), transform(board, 1)].each do |board|
-      winner ||= check_columns(board)
+
+    show(rotate(board))   { puts "Checking columns..." }
+    winner ||= check_rows(rotate(board))
+    
+    [tilt(board, -1), tilt(board, 1)].each do |board|
+      show(rotate(board)) { puts "Checking diagonals..." }
+      winner ||= check_rows(rotate(board))
     end
-    return winner
+    
+    puts "Winner: #{winner ? winner : 'nil' }"
+    winner
   end
 
   private
 
   def transform_into_2d(board_string)
-    # board string is a string of 0, 1 or 2
-    # returns 2d array
+    board = []
+    board_string.split("").each_slice(7) { |row| board << row}
+    board.map! { |row| row.map(&:to_i) }
   end
 
-  def check_rows(board)
-    # board is a 2d array of 0, 1, or 2
-    # returns nil or winner number (1 or 2)
+  def check_rows(board) 
+    winner = nil
+    [1, 2].each do |p|
+      board.each do |row|
+        if row.each_cons(4).select {|row_fragment| row_fragment == [p,p,p,p]}.any?
+          winner = p
+        end
+      end
+    end
+    winner
   end
 
-  def check_columns(board)
-    # board is a 2d array of 0, 1, or 2
-    # returns nil or winner number (1 or 2)
+  def rotate(board)
+    rotated_board = []
+    board[0].length.times { |i| rotated_board << board.map { |row| row[i] } }
+    rotated_board
   end
 
-  def transform(board, direction)
-    # board is a 2d array of 0, 1, or 2
-    # direction is -1 or 1 (indicating left or right)
-    # returns 2d array that is compatible with check_columns.
-    # it'll probably have to fill in lots of zeros.
+  def tilt(board, direction)
+    tilt_factor = (0..board.length - 1).to_a
+    tilt_factor.reverse! if direction == 1
+    board.map.with_index do |row, i|
+      tilt = tilt_factor[i]
+      [0] * tilt + row + [0] * (board.length - 1 - tilt)
+    end
+  end
+
+  def show(board)
+    yield
+    board.each do |row|
+      puts row.join('  ')
+      puts
+    end
+    puts
   end
 end
 
+
+board_string = '000000000120000022210001122000121200211122'
+Game.new.winner?(board_string)
 __END__
-
-figure when a player has won
-we'll need all the cells in the database
-they'll probably associated into locations from 0-41 based on div data- attribute
-we need to be able to do board transform_left and transform right
-we need to check columns, rows for sets of 4
-We need to return nil or winning username
-
-
-the DOM will have class of player1, player2
-
-
-0 = empty div
-1 = div of class player 1
-2 = div of class player 2
-
-we'll be sent string, read directly from the DOM, in JS.
-We'll parse string into 2d array.
-
 
 0 0 0 0 0 0 0 
 0 0 1 2 0 0 0 
 0 0 2 2 2 1 0 
 0 0 1 1 2 2 0 
 0 0 1 2 1 2 0 
-0 2 1 1 1 2 1 
+0 2 1 1 1 2 2 
