dfd8081770f1dc604591cb286f3d94eeaf79eb3b
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-21 11:56:32 -0500

    initial board css, click divs to change color

---
+++ public/js/game.js
@@ -0,0 +1,12 @@
+// <div id="#" class="slot"><p class="hidden">0</p></div>
+
+
+$(document).ready(function() {
+  // for (var i=0; i < 42; i++) {
+
+  // }
+  $('.slot').click(function() {
+    $(this).addClass("slot-red");
+  });
+
+});
