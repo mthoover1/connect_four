77c08702cc15d0403aadc52a000dfb34411f7851
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-21 09:52:48 -0500

    first commit - skeleton

---
+++ app/controllers/index.rb
@@ -0,0 +1,31 @@
+get '/' do
+  erb :index
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
+end
+
+get '/signup' do
+  erb :signup
+end
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  if User.authenticated?(params[:user][:username], params[:user][:password])
+    session[:id] = User.find_by_username(params[:user][:username]).id
+    redirect '/'
+  else
+    redirect '/login'
+  end
+end
+
+post '/createuser' do 
+  user = User.create_user(params[:user])
+  session[:id] = user.id
+  redirect '/'
+end
